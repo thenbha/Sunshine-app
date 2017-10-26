bbb5b8244ec684ac6e6f3e82eb5bfb3dd8604db8
thenbha <32986541+thenbha@users.noreply.github.com>
2017-10-26 17:17:29 -0500

    Add Refresh menu item and permission
    - Add permission for internet in the manifest file
    - Create a menu layout for the fragment to get a 'Refresh' menu item
    - Change the ForecastFragment to include the menu

--- Sunshine/app/src/main/java/com/project/home/sunshine/ForecastFragment.java
+++ Sunshine/app/src/main/java/com/project/home/sunshine/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -25,6 +28,29 @@ import java.util.ArrayList;
 public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if(itemId == R.id.action_refresh) {
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -106,6 +132,8 @@ public class ForecastFragment extends Fragment {
                 return null;
             }
             forecastJsonStr = buffer.toString();
+            Log.v(LOG_TAG,"Forecast JSON is : " + forecastJsonStr);
+
         } catch (IOException e) {
             Log.e(LOG_TAG, "Error ", e);
             // If the code didn't successfully get the weather data, there's no point in attemping
