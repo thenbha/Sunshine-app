a20fde9bfa32d104c35adbdd787da5958759a002
thenbha <32986541+thenbha@users.noreply.github.com>
2017-10-26 10:43:13 -0500

    Added List Item layout and Adapter
    Added a List Item Layout (list_item_forecast.xml)
    Created dummy data in an Arraylist and created an ArrayAdapter to display the list.

--- Sunshine/app/src/main/java/com/project/home/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/project/home/sunshine/MainActivity.java
@@ -8,6 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -57,6 +61,26 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            // Create a dummy data set to show in the ListView
+            ArrayList<String> weatherList = new ArrayList<>();
+            weatherList.add("Mon 6/23 - Sunny - 31/17");
+            weatherList.add("Tue 6/24 - Foggy - 21/8");
+            weatherList.add("Wed 6/25 - Cloudy - 22/17");
+            weatherList.add("Thurs 6/26 - Rainy - 18/11");
+            weatherList.add("Fri 6/27 - Foggy - 21/10");
+            weatherList.add("Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18");
+            weatherList.add("Sun 6/29 - Sunny - 20/7");
+
+            //Create the Array Adapter for the ListView
+            ArrayAdapter forecastAdapter = new ArrayAdapter(getActivity(),
+                    R.layout.list_item_forecast,        //list item layout
+                    R.id.list_item_forecast_textview,   //textview
+                    weatherList);
+
+            // Get the ListView and set the Adapter
+            ListView forecastView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            forecastView.setAdapter(forecastAdapter);
 
             return rootView;
         }
